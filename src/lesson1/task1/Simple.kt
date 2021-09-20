b23cbc86648675ae386d95e94606e1e4568841fd
sofisem <ssssmnv@icloud.com>
2021-09-20 22:06:01 +0300

    correction lesson 1 and lesson2 task1

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -74,7 +74,8 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = hours * 3600 + minute
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (sagenes * 48 * 4.445 + arshins * 16 * 4.445 + vershoks * 4.445)/100
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double =
+    (sagenes * 48 * 4.445 + arshins * 16 * 4.445 + vershoks * 4.445)/100
 
 /**
  * Тривиальная (1 балл)
@@ -107,7 +108,8 @@ fun thirdDigit(number: Int): Int = (number % 1000)/100
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = (hoursArrive - hoursDepart - 1) * 60 + 60 - minutesDepart + minutesArrive
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int =
+    (hoursArrive - hoursDepart - 1) * 60 + 60 - minutesDepart + minutesArrive
 
 /**
  * Простая (2 балла)
--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -3,6 +3,8 @@
 package lesson2.task1
 
 import lesson1.task1.discriminant
+import lesson1.task1.sqr
+import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.sqrt
 
@@ -68,7 +70,14 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
+fun ageDescription(age: Int): String {
+    return when {
+        age in (11..14) || age in (111..114) -> "$age лет"
+        age % 10 == 1 -> "$age год"
+        age % 10 in (2..4) -> "$age года"
+        else -> "$age лет"
+    }
+}
 
 /**
  * Простая (2 балла)
@@ -81,7 +90,15 @@ fun timeForHalfWay(
     t1: Double, v1: Double,
     t2: Double, v2: Double,
     t3: Double, v3: Double
-): Double = TODO()
+): Double {
+    val HalfWay = (v1 * t1 + v2 * t2 + v3 * t3)/2
+    return when {
+        HalfWay < t1 * v1 -> HalfWay / v1
+        HalfWay < t1 * v1 + t2 * v2 -> t1 + (HalfWay - t1 * v1) / v2
+        else -> t1 + t2 + (HalfWay - t1 * v1 - t2 * v2) / v3
+
+    }
+}
 
 /**
  * Простая (2 балла)
@@ -96,7 +113,19 @@ fun whichRookThreatens(
     kingX: Int, kingY: Int,
     rookX1: Int, rookY1: Int,
     rookX2: Int, rookY2: Int
-): Int = TODO()
+): Int {
+    return when {
+        (rookX1 == kingX) && (rookX2 == kingX) -> 3
+        (rookY1 == kingY) && (rookY2 == kingY) -> 3
+        (rookY1 == kingY) && (rookX2 == kingX) -> 3
+        (rookX1 == kingX) && (rookY2 == kingY) -> 3
+        rookX1 == kingX -> 1
+        rookX2 == kingX -> 2
+        rookY1 == kingY -> 1
+        rookY2 == kingY -> 2
+        else -> 0
+    }
+}
 
 /**
  * Простая (2 балла)
@@ -112,7 +141,15 @@ fun rookOrBishopThreatens(
     kingX: Int, kingY: Int,
     rookX: Int, rookY: Int,
     bishopX: Int, bishopY: Int
-): Int = TODO()
+): Int {
+    return when {
+        (rookX == kingX) && abs(kingX - bishopX ) == abs(kingY-bishopY) -> 3
+        (rookY == kingY) && (abs(kingX - bishopX ) == abs(kingY - bishopY)) -> 3
+        (rookX == kingX) || (rookY == kingY) -> 1
+        abs(kingX-bishopX) == abs(kingY-bishopY) -> 2
+        else -> 0
+    }
+}
 
 /**
  * Простая (2 балла)
@@ -132,4 +169,15 @@ fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
  * Найти длину пересечения отрезков AB и CD.
  * Если пересечения нет, вернуть -1.
  */
-fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int = TODO()
+fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int {
+    return when {
+        (a < d) && (b > c) && (a < c) && (b < d) -> b - c
+        (a < c) && (a < d) && (d < b) && (d > a) -> d - c
+        (a < d) && (b > c) && (b > d) -> d-a
+        (c < a) && (b < d) -> b-a
+        (c > a) && (d < b) -> d-c
+        (a < d) && (b > c) && (a > c) -> b - a
+        (a < d) && (b == c) -> 0
+        else -> -1
+    }
+}
