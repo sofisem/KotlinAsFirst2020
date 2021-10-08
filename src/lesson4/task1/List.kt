15b4643cfdd6e4140e8ae0e8ede50fbc72351fc3
sofisem <ssssmnv@icloud.com>
2021-10-08 12:17:28 +0300

    Update List.kt

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -120,14 +120,17 @@ fun buildSumExample(list: List<Int>) = list.joinToString(separator = " + ", post
  * по формуле abs = sqrt(a1^2 + a2^2 + ... + aN^2).
  * Модуль пустого вектора считать равным 0.0.
  */
-fun abs(v: List<Double>): Double = TODO()
+fun abs(v: List<Double>): Double = sqrt(v.map { it * it }.sum())
 
 /**
  * Простая (2 балла)
  *
  * Рассчитать среднее арифметическое элементов списка list. Вернуть 0.0, если список пуст
  */
-fun mean(list: List<Double>): Double = TODO()
+fun mean(list: List<Double>): Double {
+    if (list.size == 0) return 0.0
+    else return list.sum() / list.size
+}
 
 /**
  * Средняя (3 балла)
@@ -137,7 +140,13 @@ fun mean(list: List<Double>): Double = TODO()
  *
  * Обратите внимание, что данная функция должна изменять содержание списка list, а не его копии.
  */
-fun center(list: MutableList<Double>): MutableList<Double> = TODO()
+fun center(list: MutableList<Double>): MutableList<Double> {
+    val half = list.sum() / list.size
+    for (i in 0 until list.size) {
+        list[i] -= half
+    }
+    return list
+}
 
 /**
  * Средняя (3 балла)
